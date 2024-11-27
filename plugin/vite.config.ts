81b4e3f7d2961b7ae97f87abbd5046a8ca783399
Igor Aleksanov <popzxc@yandex.ru>
2024-11-27 15:01:04 +0400

    fix: Version management

--- plugin/vite.config.ts
+++ plugin/vite.config.ts
@@ -3,8 +3,12 @@ import path from 'path'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import checker from 'vite-plugin-checker'
+import { execSync } from 'child_process'
 
 export default defineConfig(() => {
+  const commitHash = execSync('git rev-parse --short HEAD').toString().trimEnd();
+  process.env.VITE_COMMIT_HASH = commitHash;
+
   return {
     define: {
       'process.env': {}
